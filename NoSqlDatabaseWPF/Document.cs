83db9cd83f63bfea1b33d6e7c336281320211576
Андрій Іл'ющенко <137763905+asiliushchenko@users.noreply.github.com>
2024-06-30 10:52:31 +0300

    Add UI

---
+++ NoSqlDatabaseWPF/Document.cs
@@ -0,0 +1,9 @@
+namespace NoSqlDatabaseWPF
+{
+    public class Document
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ NoSqlDatabaseWPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using LiteDB;
+
+namespace NoSqlDatabaseWPF
+{
+    public partial class MainWindow : Window
+    {
+        private readonly LiteDatabase _database;
+        private readonly ILiteCollection<Document> _documentsCollection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _database = new LiteDatabase(@"MyData.db");
+            _documentsCollection = _database.GetCollection<Document>("documents");
+        }
+
+        private void AddDocument_Click(object sender, RoutedEventArgs e)
+        {
+            var document = new Document
+            {
+                Id = Guid.NewGuid(),
+                Name = NameTextBox.Text,
+                Description = DescriptionTextBox.Text
+            };
+            _documentsCollection.Insert(document);
+            MessageBox.Show("Документ успішно доданий!");
+            NameTextBox.Clear();
+            DescriptionTextBox.Clear();
+        }
+
+        private void ViewDocuments_Click(object sender, RoutedEventArgs e)
+        {
+            var documents = _documentsCollection.FindAll().ToList();
+            DocumentsListBox.Items.Clear();
+            foreach (var doc in documents)
+            {
+                DocumentsListBox.Items.Add($"{doc.Name}: {doc.Description}");
+            }
+        }
+    }
+}
